1b42d883a1ed3cae1ec3c6ea247c9348126595b9
hahalek <65191020+hahalek@users.noreply.github.com>
2025-05-19 13:54:32 +0200

    Delete build/lib directory

--- build/lib/tracker/environment.py
+++
@@ -1,9 +0,0 @@
-import os.path
-from platformdirs import user_data_dir
-appname = 'tracker'
-appauthor = 'hahalek'
-
-user_dir = user_data_dir(appname, appauthor)
-
-id_filepath = os.path.join(user_data_dir(appname, appauthor), 'id.txt')
-tasks_filepath = os.path.join(user_data_dir(appname, appauthor), 'tasks.json')
